e4387f6cfe585f4254bb18a8f988016d382a8134
Steven Matchett <steve.matchett@move.com>
2019-10-10 10:37:40 -0400

    event click

--- mobileApp/src/eventList.tsx
+++ mobileApp/src/eventList.tsx
@@ -1,6 +1,9 @@
 import {BroadcastedEvent, Service} from './service';
 import React, { Component } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import {EventDetails} from './eventDetails'
+import { NavigationActions } from 'react-navigation';
 
 const styles = StyleSheet.create({
     container: {
@@ -28,6 +31,10 @@ interface State {
     communications: Array<BroadcastedEvent>;
 }
 
+const MainNavigator = createStackNavigator({
+    event: {screen: EventDetails}
+});
+
 
   
 
@@ -41,11 +48,12 @@ export class EventList extends React.Component<Props,State>{
     render() {
         console.log("HEREEE")
         console.log(this.state.communications)
+
         return (
           <View style={styles.container}>
             <FlatList
               data={this.state.communications}
-              renderItem={({item}) => <Text style={styles.item} > {item.subject} </Text>}
+              renderItem={({item}) => <Text onPress={() => {}} style={styles.item} > {item.subject} </Text>}
             />
         </View>);
     }   
