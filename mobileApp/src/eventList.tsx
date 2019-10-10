db23ca814d38eeb0328e823b6e63c13a6fb2f261
Biff <biff.byrd@move.com>
2019-10-10 14:28:44 +0000

    tslint --fix

--- mobileApp/src/eventList.tsx
+++ mobileApp/src/eventList.tsx
@@ -1,19 +1,18 @@
-import {BroadcastedEvent, Service} from './service';
-import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import {BroadcastedEvent, Service} from "./service";
 
 const styles = StyleSheet.create({
     container: {
      flex: 1,
-     paddingTop: 22
+     paddingTop: 22,
     },
     item: {
       padding: 10,
       fontSize: 18,
       height: 44,
     },
-  })
-
+  });
 
 //   import Service from './service';
 //   import Communication from './service';
@@ -21,26 +20,23 @@ const styles = StyleSheet.create({
 //   import {Button, StyleSheet, Text, View} from 'react-native';
 
 export interface Props {
-    communications: Array<BroadcastedEvent>;
+    communications: BroadcastedEvent[];
 }
 
 interface State {
-    communications: Array<BroadcastedEvent>;
+    communications: BroadcastedEvent[];
 }
 
-
-  
-
-export class EventList extends React.Component<Props,State>{
-    constructor(props){
+export class EventList extends React.Component<Props, State> {
+    constructor(props) {
         super(props);
-        this.state = {communications: props.communications}
+        this.state = {communications: props.communications};
 
     }
 
-    render() {
-        console.log("HEREEE")
-        console.log(this.state.communications)
+    public render() {
+        console.log("HEREEE");
+        console.log(this.state.communications);
         return (
           <View style={styles.container}>
             <FlatList
@@ -48,8 +44,7 @@ export class EventList extends React.Component<Props,State>{
               renderItem={({item}) => <Text style={styles.item} > {item.subject} </Text>}
             />
         </View>);
-    }   
+    }
 }
-    
-    
+
 export default EventList;
