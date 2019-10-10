cf18dc4d00643d9f10ff1322c7939fba79fb7625
Biff <biff.byrd@move.com>
2019-10-10 14:49:50 +0000

    filled in EventDetails

--- mobileApp/src/EventDetails.tsx
+++ mobileApp/src/EventDetails.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text, View } from "react-native";
 import { BroadcastedEvent } from "./service";
 
 export interface Props {
@@ -11,10 +12,36 @@ export default class EventDetails extends React.PureComponent<Props, {}> {
     }
 
     public render() {
+        const broadCastDate = new Date(this.props.event.date);
+
+        let startDate: Date;
+        let endDate: Date;
+        const isEvent = this.props.event.start_time != null && this.props.event.end_time != null;
+        if (isEvent) {
+            startDate = new Date(this.props.event.start_time);
+            endDate = new Date(this.props.event.end_time);
+        }
+
         return (
-            <div>
-                <h1>{this.props.event.subject}</h1>
-            </div>
+            <View>
+                <View>
+                    <Text>{this.props.event.subject}</Text>
+                </View>
+                <View>
+                    <Text>{new Date(this.props.event.date)}</Text>
+                </View>
+                {isEvent &&
+                    <View>
+                        <Text>Event: {startDate} - {endDate}</Text>
+                    </View>
+                }
+                <View>
+                    <Text>--- Message ---</Text>
+                </View>
+                <View>
+                    <Text>{this.props.event.body}</Text>
+                </View>
+            </View>
         );
     }
 }
