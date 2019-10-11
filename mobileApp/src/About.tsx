a0be4250ce302d24744c265a99350bef4abe4686
Smit Thakkar <smit.thakkar@move.com>
2019-10-11 12:27:15 -0400

    add loading indicator while webview is loading

--- mobileApp/src/About.tsx
+++ mobileApp/src/About.tsx
@@ -1,12 +1,24 @@
+import { Spinner } from "native-base";
 import React from "react";
 import { View } from "react-native";
 import { WebView } from "react-native-webview";
 
 const ABOUT_URL = "https://paceenterprises.org/about/";
+
+export const ActivityIndicatorLoadingView = () => {
+  return (
+    <Spinner />
+  );
+};
+
 export const About = () => {
   return (
     <View style={{ flex: 1 }}>
-      <WebView source={{ uri: ABOUT_URL }} />
+      <WebView
+      source={{ uri: ABOUT_URL }}
+      renderLoading={() => <ActivityIndicatorLoadingView />}
+      startInLoadingState={true}
+    />
     </View>
   );
 };
--- mobileApp/src/Donate.tsx
+++ mobileApp/src/Donate.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import {View} from "react-native";
 import { WebView } from "react-native-webview";
+import { ActivityIndicatorLoadingView } from "./About";
 
 const DONATE_URL = "https://paceenterprises.org/donate/";
 export const Donate = () => {
   return (
     <View style={{flex: 1}}>
-      <WebView source={{uri: DONATE_URL}} />
+      <WebView
+        source={{uri: DONATE_URL}}
+        startInLoadingState={true}
+        renderLoading={() => <ActivityIndicatorLoadingView/>}
+        />
     </View>
   );
 };
