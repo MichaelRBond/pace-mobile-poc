e8f7e592fc3a82daba05199c7822704eced34366
Michael Bond <services@the-forgotten.org>
2019-10-11 05:41:24 -0400

    add production docker-compose

--- backend/src/config/prod.ts
+++ backend/src/config/prod.ts
@@ -9,6 +9,7 @@ export const config: Config = {
   },
   mysql: {
     ...common.mysql,
-    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQL_HOST || "",
+    password: process.env.MYSQL_PASSWORD || "",
   },
 };
